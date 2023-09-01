a6b12a4e28b485d9a5e656a5bc205ba544325e7a
mroccyen <qingshanpeng@126.com>
2023-09-01 16:57:29 +0800

    解压

---
+++ src/main/java/com/alibaba/lindorm/contest/impl/compress/ZstdUtils.java
@@ -0,0 +1,19 @@
+package com.alibaba.lindorm.contest.impl.compress;
+
+import com.github.luben.zstd.Zstd;
+import com.github.luben.zstd.ZstdDictCompress;
+import com.github.luben.zstd.ZstdDictDecompress;
+
+public class ZstdUtils {
+//    public static byte[] compress(byte[] input) {
+//        // Compress
+//        ZstdDictCompress zstdDictCompress = new ZstdDictCompress(dictionary, Zstd.defaultCompressionLevel());
+//        return Zstd.compress(input, zstdDictCompress);
+//    }
+//
+//    public static byte[] decompress(byte[] input) {
+//        // Decompress
+//        ZstdDictDecompress zstdDictDecompress = new ZstdDictDecompress(dictionary);
+//        return Zstd.decompress(input, zstdDictDecompress, input.length);
+//    }
+}
