9ecf8297b8a7d0d81be24e2d10faa921c369ade0
mroccyen <qingshanpeng@126.com>
2023-09-08 16:42:11 +0800

    写入线程改为8

--- src/main/java/com/alibaba/lindorm/contest/impl/common/CommonSetting.java
+++ src/main/java/com/alibaba/lindorm/contest/impl/common/CommonSetting.java
@@ -1,7 +1,7 @@
 package com.alibaba.lindorm.contest.impl.common;
 
 public class CommonSetting {
-    public final static Integer WRITE_THREAD_COUNT = 4;
+    public final static Integer WRITE_THREAD_COUNT = 8;
     public static final String SCHEMA_FILE = "schema.txt";
     public final static String LATEST_INDEX_FILE_NAME = "latest_index";
     /**
